ba69ed56173ed341b4eb3a0bc6bb058c193c2dd2
pixie-a <aboukhris33@gmail.com>
2023-12-25 00:50:37 +0100

    3rd/4th ht funs

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - func to add an element in the hash table
+ * @ht: pointer to hash table
+ * @key: the key to add - cannot be an empty string
+ * @value: the value associated with the key
+ *
+ * Return: 1 if succeeded, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+    hash_node_t *pkg, *new_node;
+	char *valuecopy, *keycopy;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	valuecopy = strdup(value);
+	if (!valuecopy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	pkg = ht->array[index];
+
+	while (pkg)
+	{
+		if (!strcmp(key, pkg->key))
+		{
+			free(pkg->value);
+			pkg->value = valuecopy;
+			return (1);
+		}
+		pkg = pkg->next;
+	}
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(valuecopy);
+		return (0);
+	}
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
+	new_node->key = keycopy;
+	new_node->value = valuecopy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - func to retrieve the value associeted with key in ht
+ * @ht: pointer to the hash table
+ * @key: key to retrive the value
+ *
+ * Return: the value associated with the element.
+           NULL - if the key cannot be found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t  *ptr;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+
+	while (ptr)
+	{
+		if (!strcmp(key, ptr->key))
+			return (ptr->value);
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
